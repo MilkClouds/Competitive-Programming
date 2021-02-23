d6d6e1ff8fe83360e118d4f9cab59b7951e5ced2
MilkClouds <dellrikee@gmail.com>
2021-02-24 03:48:08 +0900

    Update dijkstra.cpp

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,16 +1,12 @@
 vector<int>& dijkstra(int S) {
     priority_queue<pi> pq;
     vector<int> dist(N, 0);
-    vector<bool> vis(N, 0);
     dist[S] = 0;
     pq.push({ 0, S });
     while (!pq.empty()) {
-        do {
-            u = pq.top().y;
-            pq.pop();
-        } while (!pq.empty() && vis[u]);
-        if (vis[u])break;
-        vis[u] = 1;
+        int d, u;
+        tie(d, u) = pq.top(); pq.pop();
+        if(-d != dist[u]) continue;
         for (auto v : adj[u]) {
             int nd = dist[u] + sz(v);
             if (dist[v] < nd) {
