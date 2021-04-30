f9fe70cd1ba347b2aba826e49b7b8b44bc401a01
MilkClouds <dellrikee@gmail.com>
2021-04-30 17:52:05 +0900

    1000

--- PS/BOJ/gen.py
+++ PS/BOJ/gen.py
@@ -1,11 +1,8 @@
 import glob, os
-cnt = 0
+
 for fileName in glob.glob("milkclouds/*"):
 	with open("source code in milkclouds folder.txt", "w") as file:
 		file.write(fileName)
 	os.system("git add -A")
 	os.system("git commit -m %d" % int(fileName[11:]))
 	os.system("git push")
-	cnt += 1
-	if cnt > 3:
-		break
