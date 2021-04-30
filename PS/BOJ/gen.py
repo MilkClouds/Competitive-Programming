ed25e6970422640bf2a173f25f38ec72505f7f73
MilkClouds <dellrikee@gmail.com>
2021-04-30 17:41:06 +0900

    1000

--- PS/BOJ/gen.py
+++ PS/BOJ/gen.py
@@ -1,3 +1,8 @@
-import glob
+import glob, os
 
-print(glob.glob("milkclouds/*"))
+for fileName in glob.glob("milkclouds/*"):
+	with open("source code in milkclouds folder.txt", "w") as file:
+		file.write(fileName)
+	os.system("git add -A")
+	os.system("git commit -m %d" % int(fileName[11:]))
+os.system("git push")
