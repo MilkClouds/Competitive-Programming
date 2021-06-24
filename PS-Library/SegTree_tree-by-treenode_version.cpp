4789a4edc18861e4f5360f79964989d62f889320
MilkClouds <dellrikee@gmail.com>
2021-06-24 21:08:47 +0900

    segtree treenode ver

--- PS-Library/SegTree_tree-by-treenode_version.cpp
+++ PS-Library/SegTree_tree-by-treenode_version.cpp
@@ -6,20 +6,23 @@ class TreeNode {
 public:
     int node, l, r;
     ll value, lazy;
-    TreeNode* left, right;
+    TreeNode *left, *right;
     TreeNode(){}
-    TreeNode(int node, int l, int r, vector<ll>& v){
-        value = v[node];
-        left = right = nullptr;
-        if(r - l == 1) return;
+    TreeNode(int node, int l, int r, vector<ll>& v): node(node), l(l), r(r) {
+        if(r - l == 1){
+            left = right = nullptr;
+            value = v[node];
+            return;
+        }
         int m = l + r >> 1;
         left = new TreeNode(node * 2, l, m, v);
         right = new TreeNode(node * 2 + 1, m, r, v);
+        value = left -> value + right -> value;
     }
     void push(){
         if(lazy == 0) return;
         for(auto child: {left, right}){
-            if(child != nullptr) child.lazy += lazy;
+            if(child != nullptr) child -> lazy += lazy;
         }
         lazy = 0;
     }
@@ -32,17 +35,18 @@ public:
         }
         if(e <= l || r <= s) return;
         for(auto child: {left, right}){
-            child.update(s, e, x);
+            child -> update(s, e, x);
         }
     }
     ll query(int s, int e){
         push();
+        cout << l << " " << r << " " <<s<< " " << e << endl;
         if(s <= l && r <= e) return value;
         if(e <= l || r <= s) return 0;
         ll ret = 0;
         for(auto child: {left, right}){
-            ret += child.query(s, e);
+            ret += child -> query(s, e);
         }
         return ret;
     }
-};
+} root;
