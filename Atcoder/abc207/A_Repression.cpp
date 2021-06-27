15d30757f88bf07fc1e5243583fad0a246173dca
MilkClouds <dellrikee@gmail.com>
2021-06-28 00:27:39 +0900

    atc

---
+++ Atcoder/abc207/A_Repression.cpp
@@ -0,0 +1,31 @@
+# pragma GCC optimize ("O3")
+# pragma GCC optimize ("Ofast")
+# pragma GCC optimize ("unroll-loops")
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+#define rep(i,a,b) for(int i = (a); i < (b); i++)
+#define rep2(i,a,b) for(int i = (b) - 1; i >= (a); i--)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define eb emplace_back
+#define em emplace
+#define popcount __builtin_popcount
+#define popcountll __builtin_popcountll
+#define x first
+#define y second
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pi = pair<int, int>;
+using ti = tuple<int, int, int>;
+using pl = pair<ll, ll>;
+using tl = tuple<ll, ll, ll>;
+
+ll A[3];
+int main() {
+    cin.tie(0) -> sync_with_stdio(false); cout.tie(0);
+    rep(i, 0, 3) cin >> A[i];
+    sort(A, A + 3);
+    cout << A[1] + A[2] << endl;
+}
