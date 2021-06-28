e42561f01c5c28dfd617671c8bdc8b6c927f2e53
MilkClouds <dellrikee@gmail.com>
2021-06-28 10:00:35 +0900

    agc054

---
+++ Atcoder/agc054/C_Roughly_Sorted.cpp
@@ -0,0 +1,70 @@
+# pragma GCC optimize ("O3")
+# pragma GCC optimize ("Ofast")
+# pragma GCC optimize ("unroll-loops")
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+#define rep(i,a,b) for(int i = (a); i < (b); i++)
+#define rep2(i,a,b) for(int i = (b) - 1; i >= (a); i--)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define eb emplace_back
+#define em emplace
+#define popcount __builtin_popcount
+#define popcountll __builtin_popcountll
+#define x first
+#define y second
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pi = pair<int, int>;
+using ti = tuple<int, int, int>;
+using pl = pair<ll, ll>;
+using tl = tuple<ll, ll, ll>;
+
+const ll MOD = 998244353;
+template<class T> struct mi {
+	T val;
+	mi() { val = 0; }
+	mi(const ll& v) {
+		val = (-MOD <= v && v <= MOD) ? v : v % MOD;
+		if (val < 0) val += MOD;
+	}
+	mi operator + (const mi& m) {
+		T tmp = (val + m.val) % MOD;
+		return tmp + (tmp < 0 ? MOD : 0);
+	}
+	mi operator - (const mi& m) {
+		T tmp = (val - m.val) % MOD;
+		return tmp + (tmp < 0 ? MOD : 0);
+	}
+	mi operator * (const mi& m) {
+		T tmp = (val * m.val) % MOD;
+		return tmp;
+	}
+	mi& operator += (const mi& m) {
+		if ((val += m.val) >= MOD) val -= MOD;
+		return *this;
+	}
+	mi& operator -= (const mi& m) {
+		if ((val -= m.val) < 0) val += MOD;
+		return *this;
+	}
+	mi& operator *= (const mi& m) {
+		val = (val * m.val) % MOD;
+		return *this;
+	}
+};
+mi<ll> ans = 1;
+int N, K, P[5001];
+int main() {
+    cin.tie(0) -> sync_with_stdio(false); cout.tie(0);
+    cin >> N >> K;
+    rep(i, 0, N) cin >> P[i];
+    rep(i, 0, N) {
+        int cnt = 0;
+        rep(j, 0, i) if(P[i] < P[j]) cnt++;
+        if(cnt == K) ans *= N - i;
+    }
+    cout << ans.val << endl;
+}
