15ceab97d9892057596ca01c40c1c86eff338b63
MilkClouds <dellrikee@gmail.com>
2021-06-29 16:39:20 +0900

    .

--- _.cpp
+++ _.cpp
@@ -23,23 +23,26 @@ using pl = pair<ll, ll>;
 using tl = tuple<ll, ll, ll>;
 
 const int MAX = 1e5 + 10;
+const ll INF = 6e18;
 int N, K;
-ll A[MAX], dp[MAX], S[MAX];
+ll A[MAX], dp[MAX][205], S[MAX], pre[MAX][205];
 
 typedef pair<ll, ll> Line;
 struct LiChaoTree{
-    ll f(ll i, ll j, ll k){
-        return dp[k] + (S[i] - S[j]) * (S[j] - S[k]);
+    ll f(ll j, ll i){
+        return dp[j][num] + (S[N] - S[j]) * (S[N] - S[i]);
     }
     struct Node{
         ll lnode, rnode, l;
         ll xl, xr;
     };
     vector<Node> nodes;
+    ll num;
     LiChaoTree(): LiChaoTree(0, 0) {}
-    LiChaoTree(ll xmin, ll xmax) {init(xmin, xmax);}
-    void init(ll xmin, ll xmax){
-        nodes.push_back({-1,-1,0,xmin,xmax});
+    LiChaoTree(ll xmin, ll xmax) {init(xmin, xmax, 0);}
+    void init(ll xmin, ll xmax, ll n) {
+        num = n;
+        nodes.push_back({-1,-1,N,xmin,xmax});
     }
 
     void insert(ll n, ll i){
@@ -62,7 +65,7 @@ struct LiChaoTree{
             nodes[n].l = llow;
             if( nodes[n].rnode == -1 ){
                 nodes[n].rnode = nodes.size();
-                nodes.push_back({-1,-1,0,xm+1,xr});
+                nodes.push_back({-1,-1,N,xm+1,xr});
             }
             insert(nodes[n].rnode, lhigh);
         }
@@ -73,24 +76,47 @@ struct LiChaoTree{
             nodes[n].l = lhigh;
             if( nodes[n].lnode == -1 ){
                 nodes[n].lnode = nodes.size();
-                nodes.push_back({-1,-1,0,xl,xm});
+                nodes.push_back({-1,-1,N,xl,xm});
             }
             insert(nodes[n].lnode, llow);
         }
     }
     ll ans;
-    void get(ll n, ll xq){
-        if( n == -1 ) return;
+    ll get(ll n, ll xq){
+        if( n == -1 ) return INF;
         ll xl = nodes[n].xl, xr = nodes[n].xr;
         ll xm = (xl + xr) >> 1;
-        if(f(nodes[n].l, xq) < f(ans, xq)) ans = nodes[n].l;
-        if( xq < xm ) get(nodes[n].lnode, xq);
-        else if(xq > xm) get(nodes[n].rnode, xq);
+        if(f(nodes[n].l, xq) < f(ans, xq)){
+            ans = nodes[n].l;
+        }
+        if( xq < xm ) return min(f(nodes[n].l, xq), get(nodes[n].lnode, xq));
+        else if(xq > xm) return min(f(nodes[n].l, xq), get(nodes[n].rnode, xq));
+        return f(nodes[n].l, xq);
     }
 };
 
+LiChaoTree tree[205];
 int main() {
     cin.tie(0) -> sync_with_stdio(false); cout.tie(0);
     cin >> N >> K;
     rep(i, 0, N) cin >> A[i];
+    rep(i, 0, N) S[i + 1] = S[i] + A[i];
+    rep(i, 0, K + 1) tree[i].init(0, N, i);
+    tree[0].insert(0, N);
+    tree[3]
+    rep2(i, 0, N) rep2(j, 0, K){
+        tree[j].ans = N;
+        dp[i][j + 1] = tree[j].get(0, i);
+        cout << i << " " << j + 1 << " " << dp[i][j + 1] << endl;
+        pre[i][j + 1] = tree[j].ans;
+        tree[j + 1].insert(0, i);        
+    }
+    rep(i, 0, N) {rep(j, 0, K + 1) cout << dp[i][j] << " "; cout << endl;}
+    int i = 0, j = K;
+    cout << tree[K].get(0, 0) << endl;
+    while(j){
+        cout << i << " ";
+        i = pre[i][j--];
+    }
+    cout << endl;
 }
