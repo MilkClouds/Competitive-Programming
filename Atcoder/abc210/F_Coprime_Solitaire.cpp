ce3bcebaa28cf6b148e6537ac05896c7d5d13aec
MilkClouds <dellrikee@gmail.com>
2021-07-21 00:52:47 +0900

    abc210 F. LSTM과 비슷한 발상의 2-SAT 문제

--- Atcoder/abc210/F_Coprime_Solitaire.cpp
+++ Atcoder/abc210/F_Coprime_Solitaire.cpp
@@ -2,6 +2,7 @@
 # pragma GCC optimize ("Ofast")
 # pragma GCC optimize ("unroll-loops")
 #include <bits/stdc++.h>
+#include <atcoder/all>
 #define rep(i,a,b) for(int i = (a); i < (b); i++)
 #define rep2(i,a,b) for(int i = (b) - 1; i >= (a); i--)
 #define all(x) (x).begin(), (x).end()
@@ -22,57 +23,25 @@ using pl = pair<ll, ll>;
 using tl = tuple<ll, ll, ll>;
 
 /*
-unsolved
 https://atcoder.jp/contests/abc210/tasks/abc210_f
 primes less than 2e6: 148933
-prime less than 1414: 223
 */
-const ll MAX = 3e6;
-vector<ll> pn;
-ll arr[MAX];
+
+const int MAXN = 3e4, MAXX = 2e6 + 1, MAXP = 148933;
+vector<int> pn, pr[MAXP];
+int N, arr[MAXX], A[MAXN * 2];
 void eratos() {
-	for (ll i = 2; i < MAX; i++) {
+	for (int i = 2; i < MAXX; i++) {
 		if (arr[i]) continue;
 		pn.push_back(i);
-		for (ll j = i; j < MAX; j += i) arr[j] = i;
+		for (int j = i; j < MAXX; j += i) arr[j] = i;
 	}
 }
-set<ll> decom(ll N){
-    set<ll> ret;
+set<int> decom(int N){
+    set<int> ret;
     while(N > 1){
         ret.insert(lower_bound(all(pn), arr[N]) - pn.begin());
         N /= arr[N];
-    }
-    return ret;
-}
-int N, A[60001];
-int chk[2000001];
-vector<int> pr[148933];
-bool dfs(int u){
-    bool ret = 0;
-    int cnt = 0;
-    for(auto v: decom(A[u])){
-        for(auto k: pr[v]){
-            chk[k]++; cnt++;
-            if(chk[k ^ 1]) {
-                for(auto v: decom(A[u])){
-        for(auto k: pr[v]){
-            chk[k]--;
-            if((--cnt) == 0) {
-                return 0;
-            }
-        }
-    }
-                
-            }
-        }
-    }
-    int nxt = (u / 2 + 1);
-    ret = dfs(2 * nxt) || dfs(2 * nxt + 1);
-    for(auto v: decom(A[u])){
-        for(auto k: pr[v]){
-            chk[k]--;
-        }
     }
     return ret;
 }
@@ -80,10 +49,29 @@ int main() {
     cin.tie(0) -> sync_with_stdio(false); cout.tie(0);
     eratos();
     cin >> N;
+    atcoder::two_sat ts(30 * N);
     rep(i, 0, N) cin >> A[i * 2] >> A[i * 2 + 1];
     rep(i, 0, N){
         for(auto v: decom(A[i * 2])) pr[v].pb(2 * i);
         for(auto v: decom(A[i * 2 + 1])) pr[v].pb(2 * i + 1);
+        ts.add_clause(2 * i, 1, 2 * i + 1, 1);
     }
-    cout << (dfs(0) || dfs(1) ? "Yes" : "No") << endl;
+    int S = 2 * N;
+    rep(p, 0, MAXP){
+        if(pr[p].empty()) continue;
+        rep(i, 0, pr[p].size()){
+            int Y = S++, Z = S++;
+            ts.add_clause(pr[p][i], 1 ^ 1, Y, 1);
+            ts.add_clause(pr[p][i], 1 ^ 1, Z, 1);
+            if(i){
+                ts.add_clause(Z, 1 ^ 1, Z - 2, 1);
+                ts.add_clause(Y - 2, 1 ^ 1, pr[p][i], 0);
+            }
+            if(i < pr[p].size() - 1){
+                ts.add_clause(Y, 1 ^ 1, Y + 2, 1);
+                ts.add_clause(Z + 2, 1 ^ 1, pr[p][i], 0);
+            }
+        }
+    }
+    cout << (ts.satisfiable() ? "Yes" : "No") << endl;
 }
