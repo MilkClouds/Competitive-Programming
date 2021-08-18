2fbc3f12627dcdf4d5aa5126385c87542b067890
MilkClouds <dellrikee@gmail.com>
2021-08-18 14:18:03 +0900

    fenwick

---
+++ BOJ/milkclouds/17409/17409.cpp
@@ -0,0 +1,78 @@
+# pragma GCC optimize ("O3")
+# pragma GCC optimize ("Ofast")
+# pragma GCC optimize ("unroll-loops")
+#include <bits/stdc++.h>
+#define rep(i,a,b) for(int i = (a); i < (b); i++)
+#define rep2(i,a,b) for(int i = (b) - 1; i >= (a); i--)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define eb emplace_back
+#define em emplace
+#define popcount __builtin_popcount
+#define popcountll __builtin_popcountll
+#define x first
+#define y second
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pi = pair<int, int>;
+using ti = tuple<int, int, int>;
+using pl = pair<ll, ll>;
+using tl = tuple<ll, ll, ll>;
+
+const int MOD = 1e9 + 7, MAX = 1e5 + 1;
+template<class T> struct mi {
+	T val;
+	mi() { val = 0; }
+	mi(const ll& v) {
+		val = (-MOD <= v && v <= MOD) ? v : v % MOD;
+		if (val < 0) val += MOD;
+	}
+	mi operator + (const mi& m) {
+		T tmp = (val + m.val) % MOD;
+		return tmp + (tmp < 0 ? MOD : 0);
+	}
+	mi operator - (const mi& m) {
+		T tmp = (val - m.val) % MOD;
+		return tmp + (tmp < 0 ? MOD : 0);
+	}
+	mi operator * (const mi& m) {
+		T tmp = (val * m.val) % MOD;
+		return tmp;
+	}
+	mi& operator += (const mi& m) {
+		if ((val += m.val) >= MOD) val -= MOD;
+		return *this;
+	}
+	mi& operator -= (const mi& m) {
+		if ((val -= m.val) < 0) val += MOD;
+		return *this;
+	}
+	mi& operator *= (const mi& m) {
+		val = (val * m.val) % MOD;
+		return *this;
+	}
+};
+mi<ll> tree[51][MAX + 1]; 
+int N, K, a;
+void update(mi<ll>* tree, int i, mi<ll> x){
+    for(; i <= MAX; i += i & -i) tree[i] += x;
+}
+mi<ll> query(mi<ll>* tree, int i){
+    mi<ll> ans = 0;
+    for(; i; i &= i - 1) ans += tree[i];
+    return ans;
+}
+int main() {
+    cin.tie(0) -> sync_with_stdio(false); cout.tie(0);
+    cin >> N >> K;
+    update(tree[0], 1, 1);
+    rep(i, 0, N){
+        cin >> a;
+        rep(j, 0, K){
+            update(tree[j + 1], a + 1, query(tree[j], a));
+        }
+    }
+    cout << query(tree[K], MAX).val << endl;
+}
