8a96e28f70a85b2377fd34895d0f640e5c77f9d7
MilkClouds <milkclouds@snu.ac.kr>
2022-03-06 19:43:19 +0900

    FFT

--- BOJ/milkclouds/02417/2417.py3.py
+++ BOJ/milkclouds/02417/2417.py3.py
@@ -1,2 +1,7 @@
-import math
-print(math.ceil(int(input())**.5))
+def sqrt(x):
+    j = 0
+    for i in range(64, -1, -1):
+        if (j + (1 << i)) ** 2 <= x:
+            j += 1 << i
+    return j if j * j == x else j + 1
+print(sqrt(int(input())))
--- JavaScript.py
+++
@@ -1,5 +0,0 @@
-try:
-    A, B = map(int, input().split())
-    print(A - B)
-except:
-    print("NaN")
